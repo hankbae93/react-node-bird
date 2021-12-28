0bc9d7ab2e2633287ca3d9152e6a0bce6ec40d51
Ranja Func <seolin9340@gmail.com>
2021-12-28 19:08:02 +0900

    다이나믹 라우팅 구현 및 메타 태그 세팅

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -6,6 +6,7 @@ import {
     ADD_COMMENT_REQUEST, ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE, 
     REMOVE_POST_REQUEST, REMOVE_POST_SUCCESS, REMOVE_POST_FAILURE,
     LOAD_POSTS_REQUEST, LOAD_POSTS_SUCCESS, LOAD_POSTS_FAILURE,
+    LOAD_POST_REQUEST, LOAD_POST_SUCCESS, LOAD_POST_FAILURE,
     LIKE_POST_REQUEST, LIKE_POST_SUCCESS, LIKE_POST_FAILURE,
     UNLIKE_POST_REQUEST, UNLIKE_POST_SUCCESS, UNLIKE_POST_FAILURE,
     UPLOAD_IMAGES_REQUEST, UPLOAD_IMAGES_SUCCESS, UPLOAD_IMAGES_FAILURE,
@@ -49,6 +50,25 @@ function* unlikePost(action) {
     } catch (err) {
         yield put({
             type: UNLIKE_POST_FAILURE,
+            error: err.response.data,
+        })
+    }    
+}
+
+function loadPostAPI(data) {
+    return axios.get(`/post/${data}`)
+}
+
+function* loadPost(action) {
+    try {
+        const result = yield call(loadPostAPI, action.data);
+        yield put({
+            type: LOAD_POST_SUCCESS,
+            data: result.data
+        })       
+    } catch (err) {
+        yield put({
+            type: LOAD_POST_FAILURE,
             error: err.response.data,
         })
     }    
@@ -200,6 +220,10 @@ function* watchUnlikePost() {
     yield throttle(5000, UNLIKE_POST_REQUEST, unlikePost);
 }
 
+function* watchLoadPost() {
+    yield throttle(5000, LOAD_POST_REQUEST, loadPost);
+}
+
 function* watchLoadPosts() {
     yield throttle(5000, LOAD_POSTS_REQUEST, loadPosts);
 }
@@ -222,6 +246,7 @@ export default function* postSaga(){
         fork(watchUploadImages),        
         fork(watchLikePost),        
         fork(watchUnlikePost),        
+        fork(watchLoadPost),        
         fork(watchLoadPosts),        
         fork(watchAddPost),        
         fork(watchAddComment),        
