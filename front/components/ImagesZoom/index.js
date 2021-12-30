84840f31cd758f54b02c5fb6947a69dd3ff212a9
Ranja Func <seolin9340@gmail.com>
2021-12-30 16:30:30 +0900

    Update: imgZoom src

--- front/components/ImagesZoom/index.js
+++ front/components/ImagesZoom/index.js
@@ -1,59 +1,51 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Slick from 'react-slick';
-import { Overlay, Header, CloseBtn, SlickWrapper, ImgWrapper, Indicator, Global } from './styles';
-import { IMG_CDN } from '../../utils/CDN';
-// func`` 함수호출도됨
+
+import { Overlay, Global, Header, CloseBtn, ImgWrapper, Indicator, SlickWrapper } from './styles';
 
 const ImagesZoom = ({ images, onClose }) => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-
-    return (
-        <Overlay>
-          <Global />
-
-          <Header>
-            <h1>상세 이미지</h1>
-            <CloseBtn onClick={onClose} />
-          </Header>
-
-          <SlickWrapper>
+  const [currentSlide, setCurrentSlide] = useState(0);
+  return (
+    <Overlay>
+      <Global />
+      <Header>
+        <h1>상세 이미지</h1>
+        <CloseBtn onClick={onClose}>X</CloseBtn>
+      </Header>
+      <SlickWrapper>
+        <div>
+          <Slick
+            initialSlide={0}
+            beforeChange={(slide, newSlide) => setCurrentSlide(newSlide)}
+            infinite
+            arrows={false}
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+            {images.map((v) => (
+              <ImgWrapper key={v.src}>
+                <img src={`${v.src.replace(/\/thumb\//, '/original/')}`} alt={v.src} />
+              </ImgWrapper>
+            ))}
+          </Slick>
+          <Indicator>
             <div>
-              <Slick
-                initialSlide={0}
-                afterChange={(slide) => setCurrentSlide(slide)}
-                infinite
-                arrows={false}
-                slidesToShow={1}
-                slidesToScroll={1}
-              >
-                {images.map((v) => (
-                  <ImgWrapper key={v.src}>
-                    <img src={IMG_CDN + v.src} alt={v.src} />
-                  </ImgWrapper>
-                ))}
-              </Slick>
-              
-              <Indicator>
-                <div>
-                  {currentSlide + 1}
-                  {' '}
-                  /
-                  {images.length}
-                </div>
-              </Indicator>
+              {currentSlide + 1}
+              {' '}
+              /
+              {images.length}
             </div>
-          </SlickWrapper>
-        </Overlay>
-      );
+          </Indicator>
+        </div>
+      </SlickWrapper>
+    </Overlay>
+  );
 };
 
-
 ImagesZoom.propTypes = {
-    images: PropTypes.arrayOf(PropTypes.shape({
-      src: PropTypes.string,
-    })).isRequired,
-    onClose: PropTypes.func.isRequired,
-  };
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onClose: PropTypes.func.isRequired,
+};
 
 export default ImagesZoom;
